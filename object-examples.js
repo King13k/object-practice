958f436bf98bf2fa6cff19f8c88b7832ae37bc5d
Kevin King <kevinturingking@gmail.com>
2021-09-22 11:58:06 -0600

    add car object

---
+++ object-examples.js
@@ -0,0 +1,10 @@
+var car = {
+  name: 'Grand Cherokee',
+  bodyType: 'suv',
+  engineSize: 'V8',
+  driveTrain: 'All Wheel Drive',
+  milesPerGallon: 18,
+  towingCapacity: 5200,
+  competition: ['durango', 'rangeRover', '4Runner'],
+}
+console.log(car)
